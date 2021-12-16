f9958f5a6446b41808d50c0e6dd2b1be648029b3
Zafar Kaharov <zafarkaharov@Zafars-iMac.local>
2021-12-16 11:33:57 -0800

    Initial commit

---
+++ zk-pet-clinic/src/main/java/zafar/springframework/zkpetclinic/ZkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package zafar.springframework.zkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkPetClinicApplication.class, args);
+	}
+
+}
---
+++ zk-pet-clinic/src/test/java/zafar/springframework/zkpetclinic/ZkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package zafar.springframework.zkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
