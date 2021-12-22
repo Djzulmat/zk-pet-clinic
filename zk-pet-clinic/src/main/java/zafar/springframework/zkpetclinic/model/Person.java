5b43ba2d534875eb5d955c06da31f5a7903d713c
Djzulmat <zkaharov@gmail.com>
2021-12-22 08:21:47 -0800

    Implements POJO Data Model

---
+++ zk-pet-clinic/src/main/java/zafar/springframework/zkpetclinic/model/Person.java
@@ -0,0 +1,25 @@
+package zafar.springframework.zkpetclinic.model;
+
+public class Person {
+	
+	private String firtsName;
+	private String lastName;
+	
+	//getter and setter methods
+	public String getFirtsName() {
+		return firtsName;
+	}
+	public void setFirtsName(String firtsName) {
+		this.firtsName = firtsName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+
+}
---
+++ zk-pet-clinic/src/main/java/zafar/springframework/zkpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package zafar.springframework.zkpetclinic.model;
+
+public class PetType {
+	
+	private String name;
+	
+	//getter and setter methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
